d1d10c09cd3e2483af2ddf9f848ee6602eb16d01
crynobone <crynobone@gmail.com>
2015-01-06 21:22:16 +0800

    Add packagePath setter.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- Console/Migrations/MigrateCommand.php
+++ Console/Migrations/MigrateCommand.php
@@ -5,13 +5,32 @@ use Illuminate\Database\Console\Migrations\MigrateCommand as BaseCommand;
 class MigrateCommand extends BaseCommand
 {
     /**
+     * The path to the packages directory (vendor).
+     *
+     * @var string
+     */
+    protected $packagePath;
+
+    /**
+     * Set package path.
+     *
+     * @param  string  $packagePath
+     * @return $this
+     */
+    public function setPackagePath($packagePath)
+    {
+        $this->packagePath = $packagePath;
+
+        return $this;
+    }
+
+    /**
      * Get the path to the migration directory.
      *
      * @return string
      */
     protected function getMigrationPath()
     {
-
         $path = $this->input->getOption('path');
 
         // First, we will check to see if a path option has been defined. If it has
