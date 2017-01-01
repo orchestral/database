1f726fbea6978477a79905f4eb8ded0405e89f67
crynobone <crynobone@gmail.com>
2017-01-01 23:24:12 +0800

    Add console service provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ ConsoleServiceProvider.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Orchestra\Database;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Orchestra\Database\Console\Migrations\ResetCommand;
+use Orchestra\Database\Console\Migrations\MigrateCommand;
+use Orchestra\Database\Console\Migrations\RefreshCommand;
+use Orchestra\Database\Console\Migrations\RollbackCommand;
+
+class ConsoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register all of the migration commands.
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        $commands = ['Migrate', 'Rollback', 'Reset', 'Refresh', 'Install', 'Make', 'Status'];
+
+        // We'll simply spin through the list of commands that are migration related
+        // and register each one of them with an application container. They will
+        // be resolved in the Artisan start file and registered on the console.
+        foreach ($commands as $command) {
+            $this->{'register'.$command.'Command'}();
+        }
+
+        // Once the commands are registered in the application IoC container we will
+        // register them with the Artisan start event so that these are available
+        // when the Artisan application actually starts up and is getting used.
+        $this->commands(
+            'command.migrate',
+            'command.migrate.rollback',
+            'command.migrate.reset',
+            'command.migrate.refresh'
+        );
+    }
+
+    /**
+     * Register the "migrate" migration command.
+     *
+     * @return void
+     */
+    protected function registerMigrateCommand()
+    {
+        $this->app->singleton('command.migrate', function (Application $app) {
+            return $this->getCommandWithPackage(new MigrateCommand($app->make('migrator')));
+        });
+    }
+
+    /**
+     * Register the "rollback" migration command.
+     *
+     * @return void
+     */
+    protected function registerRollbackCommand()
+    {
+        $this->app->singleton('command.migrate.rollback', function ($app) {
+            return $this->getCommandWithPackage(new RollbackCommand($app->make('migrator')));
+        });
+    }
+
+    /**
+     * Register the "reset" migration command.
+     *
+     * @return void
+     */
+    protected function registerResetCommand()
+    {
+        $this->app->singleton('command.migrate.reset', function ($app) {
+            return $this->getCommandWithPackage(new ResetCommand($app->make('migrator')));
+        });
+    }
+
+    /**
+     * Register the "refresh" migration command.
+     *
+     * @return void
+     */
+    protected function registerRefreshCommand()
+    {
+        $this->app->singleton('command.migrate.refresh', function () {
+            return $this->getCommandWithPackage(new RefreshCommand());
+        });
+    }
+
+    /**
+     * Set package path for command.
+     *
+     * @param  object  $command
+     *
+     * @return object
+     */
+    protected function getCommandWithPackage($command)
+    {
+        $packagePath = $this->app->basePath().'/vendor';
+
+        return $command->setPackagePath($packagePath);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'command.migrate',
+            'command.migrate.rollback',
+            'command.migrate.reset',
+            'command.migrate.refresh',
+        ];
+    }
+}
--- MigrationServiceProvider.php
+++ MigrationServiceProvider.php
@@ -2,74 +2,9 @@
 
 namespace Orchestra\Database;
 
-use Illuminate\Contracts\Foundation\Application;
-use Orchestra\Database\Console\Migrations\ResetCommand;
-use Orchestra\Database\Console\Migrations\MigrateCommand;
-use Orchestra\Database\Console\Migrations\RefreshCommand;
-use Orchestra\Database\Console\Migrations\RollbackCommand;
 use Illuminate\Database\MigrationServiceProvider as ServiceProvider;
 
 class MigrationServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the "migrate" migration command.
-     *
-     * @return void
-     */
-    protected function registerMigrateCommand()
-    {
-        $this->app->singleton('command.migrate', function (Application $app) {
-            return $this->getCommandWithPackage(new MigrateCommand($app->make('migrator')));
-        });
-    }
-
-    /**
-     * Register the "rollback" migration command.
-     *
-     * @return void
-     */
-    protected function registerRollbackCommand()
-    {
-        $this->app->singleton('command.migrate.rollback', function ($app) {
-            return $this->getCommandWithPackage(new RollbackCommand($app->make('migrator')));
-        });
-    }
-
-    /**
-     * Register the "reset" migration command.
-     *
-     * @return void
-     */
-    protected function registerResetCommand()
-    {
-        $this->app->singleton('command.migrate.reset', function ($app) {
-            return $this->getCommandWithPackage(new ResetCommand($app->make('migrator')));
-        });
-    }
-
-    /**
-     * Register the "refresh" migration command.
-     *
-     * @return void
-     */
-    protected function registerRefreshCommand()
-    {
-        $this->app->singleton('command.migrate.refresh', function () {
-            return new RefreshCommand();
-        });
-    }
-
-    /**
-     * Set package path for command.
-     *
-     * @param  object  $command
-     *
-     * @return object
-     */
-    protected function getCommandWithPackage($command)
-    {
-        $packagePath = $this->app->basePath().'/vendor';
-
-        return $command->setPackagePath($packagePath);
-    }
+    //
 }
